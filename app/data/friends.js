7c1fae4e46a1e47da5ff1e904c4c4376d981b13d
Beth.BootCamp <beth.gmariam@gmail.com>
2018-08-25 20:13:20 -0400

    initial-commit

---
+++ app/data/friends.js
@@ -0,0 +1,50 @@
+// friends array to hold json objects
+
+var friendsArray = [
+    {
+        "name":"Senpier",
+        "photoLink":"http://wallpaperscraft.site/ui/images/3/WDF_2493724.jpg",
+        "score":[1,2,3,4,5,1,2,3,4,5]
+    },
+
+    {
+        "name":"Biruk",
+        "photoLink":"https://images.pexels.com/photos/356378/pexels-photo-356378.jpeg",
+        "score":[5,5,4,4,4,1,2,3,4,4]
+    },
+
+    {
+        "name":"Merry",
+        "photoLink":"http://www.britishcollegeofcaninestudies.com/wordpress/wp-content/uploads/2017/11/Online-dog-law-certificate-course.jpg",
+        "score":[2,4,1,3,5,1,1,1,3,3]
+    },
+
+    {
+        "name":"Max",
+        "photoLink":"https://cdn.pixabay.com/photo/2018/01/07/17/50/flower-3067883_960_720.jpg",
+        "score":[2,2,2,2,2,3,3,3,3,3]
+    },
+
+    {
+        "name":"Vidhi",
+        "photoLink":"http://bdfjade.com/data/out/67/5738243-panda-pictures.jpg",
+        "score":[1,1,1,1,1,5,5,5,5,5]
+    },
+
+    {
+        "name":"Chriscelle",
+        "photoLink":"https://i.ebayimg.com/images/g/iiIAAOSwzXxaQeHw/s-l300.jpg",
+        "score":[3,3,3,3,3,2,1,3,4,5]
+    },
+
+
+
+
+
+
+
+
+]
+
+module.exports= friendsArray;
+
